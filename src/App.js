5d203a54ec79157dc25c155cf07de33546a64ae8
tlanders <768713+tlanders@users.noreply.github.com>
2020-03-03 07:37:24 -0600

    modified board html layout. added status section.

--- src/App.js
+++ src/App.js
@@ -5,30 +5,37 @@ function App() {
   return (
     <div className="App">
       <div className="numbrix">
-        <div className="numbrix-row">
-          <button className="numbrix-cell">0</button>
-          <button className="numbrix-cell">1</button>
-          <button className="numbrix-cell">2</button>
-          <button className="numbrix-cell">3</button>
+        <h1>Welcome to Numbrix!</h1>
+        <div className="numbrix-status">
+          <button className="status-btn">Clear Board</button>
+          <button className="status-btn">Begin Game</button>
+          <button className="status-btn">Check Board</button>
         </div>
         <div className="numbrix-row">
-          <button className="numbrix-cell">0</button>
-          <button className="numbrix-cell">1</button>
-          <button className="numbrix-cell">2</button>
-          <button className="numbrix-cell">3</button>
+          <input className="numbrix-cell" type="text" value={0}/>
+          <input className="numbrix-cell" type="text" value={1}/>
+          <input className="numbrix-cell" type="text" value={2}/>
+          <input className="numbrix-cell" type="text" value={3}/>
         </div>
         <div className="numbrix-row">
-          <button className="numbrix-cell">0</button>
-          <button className="numbrix-cell">1</button>
-          <button className="numbrix-cell">2</button>
-          <button className="numbrix-cell">3</button>
+          <input className="numbrix-cell" type="text" value={0}/>
+          <input className="numbrix-cell" type="text" value={1}/>
+          <input className="numbrix-cell" type="text" value={2}/>
+          <input className="numbrix-cell" type="text" value={3}/>
         </div>
         <div className="numbrix-row">
-          <button className="numbrix-cell">0</button>
-          <button className="numbrix-cell">1</button>
-          <button className="numbrix-cell">2</button>
-          <button className="numbrix-cell">3</button>
+          <input className="numbrix-cell" type="text" value={0}/>
+          <input className="numbrix-cell" type="text" value={1}/>
+          <input className="numbrix-cell" type="text" value={2}/>
+          <input className="numbrix-cell" type="text" value={3}/>
         </div>
+        <div className="numbrix-row">
+          <input className="numbrix-cell" type="text" value={0}/>
+          <input className="numbrix-cell" type="text" value={1}/>
+          <input className="numbrix-cell" type="text" value={2}/>
+          <input className="numbrix-cell" type="text" value={3}/>
+        </div>
+
       </div>
     </div>
   );
