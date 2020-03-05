4fa3bc0ef063f6cb8eb2477d86ce415c06e880ae
tlanders <tlanders@users.noreply.github.com>
2020-03-04 22:54:10 -0600

    mostly pulled state up into board

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,14 +2,28 @@ import React, {Component} from 'react';
 import Cell from "./Cell";
 
 class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cells:Array(16).fill(null),
+            width: 4
+        };
+    }
+
+    renderCell(i) {
+        return (
+            <Cell value={this.state.cells[i]}/>
+        )
+    }
+
     render() {
         return (
             <div className="numbrix-board">
                 <div className="numbrix-row">
-                    <Cell/>
-                    <Cell/>
-                    <Cell/>
-                    <Cell/>
+                    {this.renderCell(0)}
+                    {this.renderCell(1)}
+                    {this.renderCell(2)}
+                    {this.renderCell(3)}
                 </div>
                 <div className="numbrix-row">
                     <Cell/>
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,23 +1,10 @@
 import React, {Component} from 'react';
 
 class Cell extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {value:props.value};
-        this.handleChange = this.handleChange.bind(this);
-    }
-
     render() {
         return (
-            <input onChange={this.handleChange} className="numbrix-cell" type="text" value={this.state.value}/>
+            <input className="numbrix-cell" type="text" value={this.props.value}/>
         );
-    }
-
-    handleChange(event) {
-        const theVal = event.target.value;
-        if(Number.isInteger(theVal)) {
-            this.setState({value: event.target.value});
-        }
     }
 }
 
