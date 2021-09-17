2bd8efcf64398ac3ddb0cdcd287e34a22898f13a
Troy Landers <tlanders@landersconsulting.biz>
2021-09-16 22:04:44 -0500

    converted Board into a function component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,74 +1,55 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Cell from "./Cell";
-import {CELL_STATE, cellChange} from "../redux/gameActions";
+import {cellChange} from "../redux/gameActions";
 import {connect} from "react-redux";
 
-class Board extends Component {
+const Board = (props) => {
 
-    constructor(props) {
-        super(props);
-        this.handleCellChange = this.handleCellChange.bind(this);
-        this.getCellsToCheck = this.getCellsToCheck.bind(this);
-    }
+    // const renderCell = (index) => {
+    //     const theCell = props.game.cells[index];
+    //     return (
+    //         <Cell key={index} value={theCell.value}
+    //               cellstate={theCell.cellstate}
+    //               onChange={(evt) => handleCellChange(index, evt)}/>
+    //     )
+    // };
 
-    renderCell(index) {
-        const theCell = this.props.game.cells[index];
-        return (
-            <Cell key={index} value={theCell.value}
-                  cellstate={theCell.cellstate}
-                  onChange={(evt) => this.handleCellChange(index, evt)}/>
-        )
-    }
+    // const getCellsToCheck = (index, width) => {
+    //     let cellsToCheck = [];
+    //     if(index > width) {
+    //         // cell above
+    //         cellsToCheck.push(index - width);
+    //     }
+    //     if(index < width * (width - 1)) {
+    //         // cell below
+    //         cellsToCheck.push(index + width);
+    //     }
+    //     if(index % width !== 0) {
+    //         // cell to left
+    //         cellsToCheck.push(index - 1);
+    //     }
+    //     if(index % width !== (width - 1)) {
+    //         cellsToCheck.push(index + 1);
+    //     }
+    //     console.log('cells to check, i=' + index + ', cells=' + cellsToCheck);
+    //     return cellsToCheck;
+    // };
 
-    getCellsToCheck(index, width) {
-        let cellsToCheck = [];
-        if(index > width) {
-            // cell above
-            cellsToCheck.push(index - width);
-        }
-        if(index < width * (width - 1)) {
-            // cell below
-            cellsToCheck.push(index + width);
-        }
-        if(index % width !== 0) {
-            // cell to left
-            cellsToCheck.push(index - 1);
-        }
-        if(index % width !== (width - 1)) {
-            cellsToCheck.push(index + 1);
-        }
-        console.log('cells to check, i=' + index + ', cells=' + cellsToCheck);
-        return cellsToCheck;
-    }
-
-    handleCellChange(index, event) {
+    const handleCellChange = (index, event) => {
         const cellValue = event.target.value;
         console.log('Board - cell change, index=' + index + ", val=" + cellValue);
-        this.props.onCellChange(index, cellValue);
+        props.onCellChange(index, cellValue);
     }
 
-    render() {
-        const {width} = this.props.game;
-        const rows = []
-        for(let i = 0; i < width; i++) {
-            rows.push(this.renderRow(i));
-        }
-        return (
-            <div className="numbrix-board">
-                {rows}
-            </div>
-        );
-    }
-
-    renderRow(rowNum) {
-        const {cells, width} = this.props.game;
+    const renderRow = (rowNum) => {
+        const {cells, width} = props.game;
         const renderCells = [];
         for(let i = 0; i < width; i++) {
             const index = rowNum * width + i;
             renderCells.push(<Cell key={index} value={cells[index].value}
-                             cellstate={cells[index].cellstate}
-                             onChange={(evt) => this.handleCellChange(index, evt)}/>)
+                                   cellstate={cells[index].cellstate}
+                                   onChange={(evt) => handleCellChange(index, evt)}/>)
         }
         return (
             <div className="numbrix-row" key={rowNum}>
@@ -76,6 +57,17 @@ class Board extends Component {
             </div>
         );
     }
+
+    const {width} = props.game;
+    const rows = []
+    for(let i = 0; i < width; i++) {
+        rows.push(renderRow(i));
+    }
+    return (
+        <div className="numbrix-board">
+            {rows}
+        </div>
+    );
 }
 
 Board.propTypes = {
