c9e50484d546e0aa1350f29c7b362a6764802d8b
Troy Landers <tlanders@landersconsulting.biz>
2021-11-13 17:57:31 -0600

    fixed start game bug

--- src/redux/gameReducer.tsx
+++ src/redux/gameReducer.tsx
@@ -3,15 +3,15 @@ import {Action, Cell, CellState, Game, GameMode} from "../types";
 
 const initialState = {
     mode: GameMode.SETUP_MODE,
-    width: 4,
-    height: 4,
+    width: 3,
+    height: 3,
     cells:Array(16).fill({value:'', cellstate:CellState.EMPTY})
 };
 
 const isConstantCellState = (cs:CellState) => cs === CellState.CONSTANT;
 const isConstantCell = (c:Cell) => isConstantCellState(c.cellstate);
-const isValidCellState = (cs:CellState) => cs === CellState.VALID;
-const isValidCell = (c:Cell) => isValidCellState(c.cellstate);
+// const isValidCellState = (cs:CellState) => cs === CellState.VALID;
+// const isValidCell = (c:Cell) => isValidCellState(c.cellstate);
 const isInvalidCellState = (cs:CellState) => cs === CellState.INVALID;
 const isInvalidCell = (c:Cell) => isInvalidCellState(c.cellstate);
 
@@ -49,52 +49,8 @@ const handleCheckBoardClick = ({cells, width, height}:Game) => {
     const newCells = cells.slice();
     let constantCells = [];
     let foundCells = [];
-    markDuplicateCells(newCells);
-    // let otherCells = [];
-    for(let row = 0; row < height; row++) {
-        for(let col = 0; col < width; col++) {
-            const cellIndex = getCellIndex(row, col, width);
-            const cellVal = Number(newCells[cellIndex].value);
-            const cellState = newCells[cellIndex].cellstate;
-
-            // console.log("cellIndex=" + cellIndex + ", row=" + row + ", cell=" + col);
-            if(cellState === CellState.CONSTANT) {
-                foundCells[cellVal] = cells[cellIndex];
-                const cellsToCheck = getCellsToCheck(cellIndex, width, height);
-                let tempCells = [];
-                constantCells.push(cellVal);
-                cellsToCheck.forEach(theIndex => {
-                    const theCellVal = Number(newCells[theIndex].value);
-                    const theCellState = newCells[theIndex].cellstate;
-                    if(theCellState === CellState.VALID) {
-                        if(theCellVal === cellVal - 1 || theCellVal === cellVal + 1) {
-
-                        }
-                    }
-                });
-            } else if(cellState === CellState.VALID) {
-                foundCells[cellVal] = cells[cellIndex];
-                const cellsToCheck = getCellsToCheck(cellIndex, width, height);
-                // compare to neighboring cells. dup cells have already been marked so
-                // now only need to verify that there is at least one neighboring cell that is
-                // +/- 1 of this cell.
-                let foundOtherCell = false;
-                cellsToCheck.forEach(theIndex => {
-                    const theCellState = cells[theIndex].cellstate;
-                    if(isValidCellState(theCellState) || isConstantCellState(theCellState)) {
-                        const theCellVal = Number(cells[theIndex].value);
-                        if(theCellVal === cellVal - 1 || theCellVal === cellVal + 1) {
-                            foundOtherCell = true;
-                        }
-                    }
-                });
-                if(!foundOtherCell) {
-                    // current cell is invalid
-                    console.log('found invalid cell: ', cellIndex);
-                    cells[cellIndex].cellstate = CellState.INVALID;
-                }
-            }
-        }
+    if(markDuplicateCells(newCells) && markInvalidCells(cells, width, height)) {
+        console.log('===== game finished successfully =====');
     }
     return newCells;
 }
@@ -171,10 +127,12 @@ const handleGameStart = ({width, height, cells}:Game) => {
 };
 
 export const anyCellsHaveValue = (cellValue: number, cells:Cell[], cellsToCheck:number[]) => {
-    const cellWithValue = cellsToCheck.find((val, index, arr) =>
-        cells[val].value === cellValue.toString() && cells[val].cellstate !== CellState.INVALID && cells[val].cellstate !== CellState.EMPTY);
-    console.log("anyHaveValue, found cellWithVal=", cellWithValue);
-    return cellWithValue !== undefined && cellWithValue > 0;
+    const cellWithValue = cellsToCheck.find(indexToCheck => (
+        cells[indexToCheck].value === cellValue.toString()
+            && cells[indexToCheck].cellstate !== CellState.INVALID
+            && cells[indexToCheck].cellstate !== CellState.EMPTY));
+    // console.log("anyHaveValue, found cellWithVal=", cellWithValue);
+    return cellWithValue !== undefined && cellWithValue >= 0;
 }
 
 export const anyCellsAreEmptyOrInvalid = (cells:Cell[], cellsToCheck:number[]) => {
@@ -215,12 +173,10 @@ const markInvalidCells = (cells:Cell[], width:number, height:number) => {
                             cell.cellstate = CellState.INVALID;
                             allValid = false;
                         }
-                    } else {
-                        if (!(anyAreMinusOne && anyArePlusOne)) {
-                            console.log(`cell ${index} is invalid`);
-                            cell.cellstate = CellState.INVALID;
-                            allValid = false;
-                        }
+                    } else if (!anyAreMinusOne || !anyArePlusOne) {
+                        console.log(`cell ${index} is invalid`);
+                        cell.cellstate = CellState.INVALID;
+                        allValid = false;
                     }
                 }
             }
