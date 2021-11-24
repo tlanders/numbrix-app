4c54e2b64c0675e57dbf7126d8d968a1685a5ffd
Troy Landers <tlanders@landersconsulting.biz>
2021-11-23 18:38:24 -0600

    basic amplify auth added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "testinglocal85f34476": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,23 @@ import React from 'react';
 import Numbrix from "./components/Numbrix";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
+import {withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App container-fluid">
         <Provider store={store}>
             <Numbrix/>
+            <div className={"container"}>
+                <div className={"row"}>
+                    <div className={"col-md-3 mx-auto mt-3"}>
+                        <AmplifySignOut />
+                    </div>
+                </div>
+            </div>
         </Provider>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
