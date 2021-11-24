5f6c20f47d7b274d09f19cc5f0fb74980377d694
Troy Landers <tlanders@landersconsulting.biz>
2021-11-24 17:25:58 -0600

    added image storage and uploads

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Numbrix from "./components/Numbrix";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
 import {withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import { API } from 'aws-amplify';
+import { API, Storage } from 'aws-amplify';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
@@ -21,12 +21,24 @@ function App() {
 
     async function fetchNotes() {
         const apiData = await API.graphql({ query: listNotes });
+        const notesFromAPI = apiData.data.listNotes.items;
+        await Promise.all(notesFromAPI.map(async note => {
+            if (note.image) {
+                const image = await Storage.get(note.image);
+                note.image = image;
+            }
+            return note;
+        }))
         setNotes(apiData.data.listNotes.items);
     }
 
     async function createNote() {
         if (!formData.name || !formData.description) return;
         await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+        if (formData.image) {
+            const image = await Storage.get(formData.image);
+            formData.image = image;
+        }
         setNotes([ ...notes, formData ]);
         setFormData(initialFormState);
     }
@@ -35,6 +47,14 @@ function App() {
         const newNotesArray = notes.filter(note => note.id !== id);
         setNotes(newNotesArray);
         await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+    }
+
+    async function onChange(e) {
+        if (!e.target.files[0]) return
+        const file = e.target.files[0];
+        setFormData({ ...formData, image: file.name });
+        await Storage.put(file.name, file);
+        fetchNotes();
     }
 
     return (
@@ -55,6 +75,10 @@ function App() {
                             placeholder="Note description"
                             value={formData.description}
                         />
+                        <input
+                            type="file"
+                            onChange={onChange}
+                        />
                         <button onClick={createNote}>Create Note</button>
                         <div style={{marginBottom: 30}}>
                             {
@@ -63,6 +87,9 @@ function App() {
                                         <h2>{note.name}</h2>
                                         <p>{note.description}</p>
                                         <button onClick={() => deleteNote(note)}>Delete note</button>
+                                        {
+                                            note.image && <img src={note.image} style={{width: 400}} />
+                                        }
                                     </div>
                                 ))
                             }
